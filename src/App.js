886025ecd079c495ac9faa57fd301ce90a299087
pfst-me <naikpavan56@gmail.com>
2020-12-18 12:27:43 +0530

    header commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Header from "./Header";
 import "./App.css";
 
 function App() {
-  return <div className="">App</div>;
+  return (
+    //BEM naming convention
+    <div className="app">
+      <Header />
+      {/** home */}
+      {/** footer */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9tVikXsjHHIhgmPWTCsbPQHU06ba0Qio",
+  authDomain: "clone-68d76.firebaseapp.com",
+  databaseURL: "https://clone-68d76.firebaseio.com",
+  projectId: "clone-68d76",
+  storageBucket: "clone-68d76.appspot.com",
+  messagingSenderId: "837613319299",
+  appId: "1:837613319299:web:ee01a1fdc0b463ae1e97b6",
+  measurementId: "G-G4MMT4JCTM",
+};
